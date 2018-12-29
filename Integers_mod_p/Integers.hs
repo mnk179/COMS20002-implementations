7b5bd29763b01d156318e6b5505b284a18812820
mnk179 <martynas.nor@gmail.com>
2018-12-29 19:48:00 +0000

    Implement subtraction

--- Integers_mod_p/Integers.hs
+++ Integers_mod_p/Integers.hs
@@ -8,3 +8,5 @@ data IntModP = IntModP Integer Integer deriving (Show, Eq)
 addModP :: IntModP -> IntModP -> IntModP
 addModP (IntModP xn xp) (IntModP yn yp) = IntModP (assert (xp == yp) ((xn + yn) `mod` xp)) xp
 
+subModP :: IntModP -> IntModP -> IntModP
+subModP (IntModP xn xp) (IntModP yn yp) = IntModP (assert (xp == yp) ((xn - yn) `mod` xp)) xp
--- Integers_mod_p/Tests.hs
+++ Integers_mod_p/Tests.hs
@@ -8,3 +8,13 @@ addTest3 = TestCase (assertEqual "for 1 + 2 (mod 4)" (addModP (IntModP 1 4) (Int
 addTests = TestList [TestLabel "Addition Test 1" addTest1,
                     TestLabel "Addition Test 2" addTest2,
                     TestLabel "Addition Test 3" addTest3]
+
+subTest1 = TestCase (assertEqual "for 2 - 3 (mod 5)" (subModP (IntModP 2 5) (IntModP 3 5)) (IntModP 4 5))
+subTest2 = TestCase (assertEqual "for 3 - 2 (mod 4)" (subModP (IntModP 3 4) (IntModP 2 4)) (IntModP 1 4))
+subTest3 = TestCase (assertEqual "for 1 - 2 (mod 4)" (subModP (IntModP 1 4) (IntModP 2 4)) (IntModP 3 4))
+
+subTests = TestList [TestLabel "Subtraction Test 1" subTest1,
+                    TestLabel "Subtraction Test 2" subTest2,
+                    TestLabel "Subtraction Test 3" subTest3]
+
+tests = TestList [addTests, subTests]
